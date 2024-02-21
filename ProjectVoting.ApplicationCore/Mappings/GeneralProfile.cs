5631fb792955f1b730fbc50794632fc2954cbc0c
vastamaa <vsztom@gmail.com>
2024-02-21 22:17:57 +0100

    Created a general mapping profile with a basic user map.

---
+++ ProjectVoting.ApplicationCore/Mappings/GeneralProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProjectVoting.ApplicationCore.DTOs;
+
+namespace ProjectVoting.ApplicationCore.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<UserRegistration, User>()
+                .ForMember(user => user.UserName, options => options.MapFrom(x => x.Email));
+        }
+    }
+}
