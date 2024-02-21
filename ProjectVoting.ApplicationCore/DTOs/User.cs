fd2ca65bd743c37fd25b2e8c70e408e6e3115378
vastamaa <vsztom@gmail.com>
2024-02-21 19:52:38 +0100

    Added new class library called ApplicationCore. Created User DTO.

---
+++ ProjectVoting.ApplicationCore/DTOs/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectVoting.ApplicationCore.DTOs
+{
+    public class UserRegistration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
