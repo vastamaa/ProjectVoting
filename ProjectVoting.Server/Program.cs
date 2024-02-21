d39784b3a42f71176fb07e8bceb326c49fa32c82
vastamaa <vsztom@gmail.com>
2024-02-21 22:18:56 +0100

    Added extensions.

--- ProjectVoting.Server/Program.cs
+++ ProjectVoting.Server/Program.cs
@@ -1,4 +1,6 @@
 
+using ProjectVoting.ApplicationCore.Extensions;
+
 namespace ProjectVoting.Server
 {
     public class Program
@@ -8,6 +10,8 @@ namespace ProjectVoting.Server
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddApplicationCore();
+            builder.Services.AddInfrastructure(builder.Configuration);
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
