58a839b7c74081c77b795ed9a51571d376fc16e8
vastamaa <vsztom@gmail.com>
2024-02-21 22:18:08 +0100

    Created DbContext.

---
+++ ProjectVoting.Infrastructure/Persistence/Contexts/Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectVoting.Infrastructure.Persistence.Contexts
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+
+    }
+}
