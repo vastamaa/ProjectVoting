79b82dfb5ea0cbcd502ed9152217878e4104a94d
vastamaa <vsztom@gmail.com>
2024-02-21 22:17:29 +0100

    Added ServiceCollectionExtensions with multiple extension methods.

---
+++ ProjectVoting.ApplicationCore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using ProjectVoting.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using ProjectVoting.ApplicationCore.DTOs;
+
+namespace ProjectVoting.ApplicationCore.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationCore(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<Context>(options => options.UseSqlServer(defaultConnectionString));
+
+            return services;
+        }
+
+        public static IServiceCollection AddUserIdentity(this IServiceCollection services)
+        {
+            services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequiredLength = 10;
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<Context>();
+
+            return services;
+        }
+    }
+}
