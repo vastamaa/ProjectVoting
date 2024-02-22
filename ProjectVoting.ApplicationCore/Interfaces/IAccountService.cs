a5eff6537f346c7b338c26ca665d5ff01a27873b
vastamaa <vsztom@gmail.com>
2024-02-22 19:12:04 +0100

    Created IAccountService regarding management with its implementation AccountService.

---
+++ ProjectVoting.ApplicationCore/Interfaces/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectVoting.ApplicationCore.DTOs;
+
+namespace ProjectVoting.ApplicationCore.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<IdentityResult> RegisterUser(UserRegistration userModel);
+        Task AddUserToRole(UserRegistration userModel, string role);
+        Task<bool> LoginAsync(UserLogin userModel);
+        Task LogoutAsync();
+
+    }
+}
---
+++ ProjectVoting.ApplicationCore/Services/AccountService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using ProjectVoting.ApplicationCore.DTOs;
+using ProjectVoting.ApplicationCore.Interfaces;
+
+namespace ProjectVoting.ApplicationCore.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountService(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task AddUserToRole(UserRegistration userModel, string role)
+        {
+            await _userManager.AddToRoleAsync(_mapper.Map<User>(userModel), role);
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserRegistration userModel)
+        {
+            return await _userManager.CreateAsync(_mapper.Map<User>(userModel), userModel.Password);
+        }
+
+        public async Task<bool> LoginAsync(UserLogin userModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userModel.Email, userModel.Password, userModel.RememberMe, lockoutOnFailure: false);
+
+            return result.Succeeded;
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
