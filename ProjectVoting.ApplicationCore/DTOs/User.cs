f0c6d800c9ac5281e9716b191f9b124eccfdd83e
vastamaa <vsztom@gmail.com>
2024-02-22 19:10:50 +0100

    Added UserLogin.

--- ProjectVoting.ApplicationCore/DTOs/User.cs
+++ ProjectVoting.ApplicationCore/DTOs/User.cs
@@ -19,8 +19,20 @@ namespace ProjectVoting.ApplicationCore.DTOs
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The passwords do not match.")]
         public string ConfirmPassword { get; set; }
+    }
 
+    public class UserLogin
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
 
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 
     public class User : IdentityUser
