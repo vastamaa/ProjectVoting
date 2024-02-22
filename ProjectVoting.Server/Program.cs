736a3bf761761018c9cfac4a58ef9586bc30846a
vastamaa <vsztom@gmail.com>
2024-02-22 22:23:40 +0100

    Important comment. Added built in ExceptionHandler. Had difficulty implementing it in a separate extension class so for now its place is within the Program.cs.

--- ProjectVoting.Server/Program.cs
+++ ProjectVoting.Server/Program.cs
@@ -1,11 +1,13 @@
-
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using ProjectVoting.ApplicationCore.DTOs;
 using ProjectVoting.ApplicationCore.Extensions;
 using ProjectVoting.Server.Filters;
+using System.Net;
 
 namespace ProjectVoting.Server
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -17,9 +19,12 @@ namespace ProjectVoting.Server
             builder.Services.AddCustomServices();
             builder.Services.AddUserIdentity();
             builder.Services.AddScoped<ValidationFilterAttribute>();
+
+            // Don't remove, else request can't reach action without this, because [ApiController] validates the models by default.
             builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -36,6 +41,8 @@ namespace ProjectVoting.Server
                 app.UseSwaggerUI();
             }
 
+            app.ConfigureExceptionHandler();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
@@ -47,5 +54,28 @@ namespace ProjectVoting.Server
 
             app.Run();
         }
+
+        #region ExceptionMiddlewareExtension
+        public static void ConfigureExceptionHandler(this IApplicationBuilder application)
+        {
+            application.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+        #endregion
     }
 }
