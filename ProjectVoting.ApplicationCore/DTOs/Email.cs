3f86a5107da856a4dff4a345e7b977317394b02d
vastamaa <vsztom@gmail.com>
2024-03-20 22:28:52 +0100

    Removed EmailOptions as it was not needed.

--- ProjectVoting.ApplicationCore/DTOs/Email.cs
+++ ProjectVoting.ApplicationCore/DTOs/Email.cs
@@ -2,15 +2,6 @@
 
 namespace ProjectVoting.ApplicationCore.DTOs
 {
-    public class EmailOptions
-    {
-        public string From { get; set; }
-        public string SmtpServer { get; set; }
-        public int Port { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-
     public class EmailMessage
     {
         public List<MailAddress> To { get; set; }
